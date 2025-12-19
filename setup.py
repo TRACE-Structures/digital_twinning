95eb2c75bb421bb3575e86e6e5bc35e4732ddaa6
Bence Popovics <47786942+popbence@users.noreply.github.com>
2025-12-19 17:20:25 +0100

    Bump version from 0.1.5 to 0.1.6

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 setup(
     name='digital_twinning',
-    version='0.1.5',
+    version='0.1.6',
     author='András Urbanics, Áron Friedman, Bence Popovics, Emese Vastag, Noémi Friedman',
     author_email='popbence@hun-ren.sztaki.hu',
     description='A comprehensive package for digital twin model updating and predictive modeling using machine learning and uncertainty quantification techniques',
