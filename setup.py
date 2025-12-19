4f1144093382a40b33f038bf2d4ae3a94961b987
Bence Popovics <47786942+popbence@users.noreply.github.com>
2025-12-19 15:47:13 +0100

    Fix package names in setup.py

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
         'asyncio',
         'catboost',
         'emcee',
-        'gPCE_model',
+        'gPCE-model',
         'IPython',
         'lightgbm',
         'matplotlib',
@@ -29,7 +29,7 @@ setup(
         'shap',
         'scikit-learn',
         'torch',
-        'uncertain_variables',
+        'uncertain-variables',
         'xgboost'
     ],
     classifiers=[
